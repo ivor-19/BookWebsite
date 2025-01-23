aab4a60f4321a2c2939dfd60472bd0ee9d745d92
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-01-23 23:43:24 +0800

    fix

--- src/app/api/test-connection/route.ts
+++ src/app/api/test-connection/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
+import {  NextResponse } from 'next/server';
 import dbConnect from "../../../lib/dbConnect";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     await dbConnect();
     return NextResponse.json({ message: 'MongoDB connection successful!' });
