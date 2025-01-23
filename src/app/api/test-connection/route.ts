4b49118921893429d5838ea71b3db5be67fff75f
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-01-23 23:41:16 +0800

    asdasd

--- src/app/api/test-connection/route.ts
+++ src/app/api/test-connection/route.ts
@@ -1,11 +1,11 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import dbConnect from "../../../lib/dbConnect";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+export async function GET(req: NextRequest) {
   try {
     await dbConnect();
-    res.status(200).json({ message: 'MongoDB connection successful!' });
+    return NextResponse.json({ message: 'MongoDB connection successful!' });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to connect to MongoDB', details: (error as Error).message });
+    return NextResponse.json({ error: 'Failed to connect to MongoDB', details: (error as Error).message }, { status: 500 });
   }
 }
